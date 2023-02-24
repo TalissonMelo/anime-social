adb34a7aa4d2c375a0e9c2e76d66c63ed78bfda7
TalissonMelo <talissonmelorodrigues@gmail.com>
2023-02-24 19:30:43 -0300

    Adicionado Spring Security

--- postagem/src/main/java/com/talissonmelo/repositorio/PostagemRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.talissonmelo.repositorio;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.talissonmelo.entidade.Postagem;
-
-public interface PostagemRepositorio  extends MongoRepository<Postagem, String> { 
-
-}
